4e940bd7ddb20c6f406afd82faeb75fc7347e9d6
BohdanRubanov <110721603+BohdanRubanov@users.noreply.github.com>
2025-12-09 12:00:27 +0200

    Co-authored-by: MilanaSatska <MilanaSatska@users.noreply.github.com>

--- modules/state_handler.py
+++ modules/state_handler.py
@@ -32,18 +32,30 @@ async def choise_handler(message: Message, state: FSMContext):
     if message.text == "Створити тест":
         await state.set_state(Teacher.questions_count)
         await message.answer(text= "Введіть кількість питань")
+
     
 @dispatcher.message(Teacher.questions_count)
 async def questions_count_handler(message: Message, state: FSMContext):
     await state.update_data(questions_count = message.text)
+    await state.update_data(questions = [])
     await state.set_state(Teacher.questions)
-    await message.answer(text="Питання потрібно вводити у відповідному вигляді:\n Номер питання. Текст питання \n Приклад:\n 1. Скільки буде 2+2?")
+    # await message.answer(text="Питання потрібно вводити у відповідному вигляді:\n Номер питання. Текст питання \n Приклад:\n 1. Скільки буде 2+2?")
     await message.answer(text="Введіть перше питання")
 
 @dispatcher.message(Teacher.questions)
 async def questions_handler(message: Message, state: FSMContext):
-    if message.text[0] == "1":
-        questions_list:list = await state.get_data()["questions"]
+    state_data = await state.get_data()
+    if int(state_data["questions_count"]) > len(state_data["questions"]):
+        questions_list:list = state_data["questions"]
         questions_list.append(message.text)
         await state.update_data(questions = questions_list)
+    else:
+        test_data = await state.get_data()
+        user_json_data = json_data()
+        user = user_json_data[f"{message.from_user.id}"]
+        user["test_list"].append(test_data["questions"])
+        user_json_data[f"{message.from_user.id}"] = user
+        save_data(user_json_data)
+
+    
 
